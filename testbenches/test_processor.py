783574f79c63bc5583ee0f93f92995e3b540424e
Saleh Ghobari <kreanal355@gmail.com>
2025-02-21 00:09:24 +0300

    fixed instruction memory output and added run.bat to automate running process

--- testbenches/test_processor.py
+++ testbenches/test_processor.py
@@ -10,6 +10,8 @@ async def processor_test(dut):
     clock = Clock(dut.clk, 10, units="ns")
     cocotb.start_soon(clock.start())
 
+
+    dut.enable.value = 1
     # Apply reset
     dut.rst.value = 0
     await cocotb.triggers.Timer(1, units="ns")  # Hold reset for a few ns
@@ -18,16 +20,14 @@ async def processor_test(dut):
     await cocotb.triggers.Timer(3, units="ns")  # Hold reset for a few ns
 
     dut.rst.value = 1
-
-    cocotb.log.info(f"rst de-asserted")
-
-
+    
     # Run simulation for 1000 ns
+    await RisingEdge(dut.clk)
+    await Timer(1, units="ns")
+
     for cycle in range(100):
         await RisingEdge(dut.clk)
         await Timer(1, units="ns")
         cocotb.log.info(f"Cycle {cycle}: PC = {int(dut.PC.value)}")
         cocotb.log.info(f"Instruction 1 = {hex(dut.instMem.q_a.value)} \n")
-        cocotb.log.info(f"Instruction 2 = {hex(dut.instMem.q_b.value)} \n")
-        cocotb.log.info(f"address stall 1 = {dut.instMem.addressstall_a.value} \n")
-        cocotb.log.info(f"address stall 2 = {dut.instMem.addressstall_b.value} \n") 
+        cocotb.log.info(f"Instruction 2 = {hex(dut.instMem.q_b.value)} \n") 
