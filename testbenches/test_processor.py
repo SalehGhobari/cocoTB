58422e75eccf01a76f4d9a4668c4f83319e6c8ed
Saleh Ghobari <kreanal355@gmail.com>
2025-02-20 19:05:27 +0300

    added .mif files for instMem and DM

--- testbenches/test_processor.py
+++ testbenches/test_processor.py
@@ -1,14 +1,33 @@
 import cocotb
-from cocotb.triggers import Timer
-from cocotb.result import TestSuccess
+from cocotb.clock import Clock
+from cocotb.triggers import RisingEdge, Timer
 
 @cocotb.test()
-async def hello_world_test(dut):
-    # Print "Hello, World!" to the console
-    dut._log.info("Hello, World!")
+async def processor_test(dut):
+    """Testbench for the processor module."""
     
-    # Wait for a small amount of time (optional)
-    await Timer(1, units='ns')
-    
-    # Raise TestSuccess to indicate that the test passed
-    raise TestSuccess("All ORGate2 test cases passed!")
+    # Create a clock with a period of 10 ns (100 MHz)
+    clock = Clock(dut.clk, 10, units="ns")
+    cocotb.start_soon(clock.start())
+
+    # Apply reset
+    dut.rst.value = 0
+    await cocotb.triggers.Timer(1, units="ns")  # Hold reset for a few ns
+    cocotb.log.info(f"rst asserted, PCin Value = {int(dut.pc.PCin.value)}")
+    cocotb.log.info(f"rst asserted, PCout Value = {int(dut.pc.PCout.value)}")
+    await cocotb.triggers.Timer(3, units="ns")  # Hold reset for a few ns
+
+    dut.rst.value = 1
+
+    cocotb.log.info(f"rst de-asserted")
+
+
+    # Run simulation for 1000 ns
+    for cycle in range(100):
+        await RisingEdge(dut.clk)
+        await Timer(1, units="ns")
+        cocotb.log.info(f"Cycle {cycle}: PC = {int(dut.PC.value)}")
+        cocotb.log.info(f"Instruction 1 = {hex(dut.instMem.q_a.value)} \n")
+        cocotb.log.info(f"Instruction 2 = {hex(dut.instMem.q_b.value)} \n")
+        cocotb.log.info(f"address stall 1 = {dut.instMem.addressstall_a.value} \n")
+        cocotb.log.info(f"address stall 2 = {dut.instMem.addressstall_b.value} \n") 
